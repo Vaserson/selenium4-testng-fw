b18ff255e66340de4f75eb8db9468a7ec76abf98
Vaserson <kadabra777@gmail.com>
2022-12-31 17:52:31 +0200

    02. ChromeDriver to setUp and tearDown methods

--- src/test/java/com/phptravelers/tests/HomeTests.java
+++ src/test/java/com/phptravelers/tests/HomeTests.java
@@ -1,36 +1,42 @@
 package com.phptravelers.tests;
 
+import constants.Constants;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class HomeTests {
+    WebDriver driver;
 
-    @Test
-    public void test1() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    @BeforeMethod
+    public void setUp() {
+
+        System.setProperty("webdriver.chrome.driver", Constants.getChromedriverpath());
+        driver = new ChromeDriver();
 
         driver.get("https://phptravels.com/");
-        driver.findElement(By.xpath("//ul[@class='jfHeader-menuList']/li/a[text()='Demo']")).click();
-        System.out.println(driver.getTitle());
-        Thread.sleep(2000);
+    }
 
+    @AfterMethod
+    public void tearDown() {
         driver.quit();
     }
 
     @Test
-    public void test2() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    public void test1() throws InterruptedException {
+        driver.findElement(By.xpath("//ul[@class='jfHeader-menuList']/li/a[text()='Demo']")).click();
+        System.out.println(driver.getTitle());
+        Thread.sleep(2000);
+    }
 
-        driver.get("https://phptravels.com/");
+    @Test
+    public void test2() throws InterruptedException {
         driver.findElement(By.xpath("//ul[@class='jfHeader-menuList']/li/a[text()='Pricing']")).click();
         System.out.println(driver.getTitle());
         Thread.sleep(2000);
-
-        driver.quit();
     }
 
 }
