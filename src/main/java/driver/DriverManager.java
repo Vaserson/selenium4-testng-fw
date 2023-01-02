1339569d9678a1c74e1e4610b1d2a5d245c2590d
Vaserson <kadabra777@gmail.com>
2023-01-02 10:13:23 +0200

    05. dr with default value

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -1,12 +1,19 @@
 package driver;
 
+import constants.Constants;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public final class DriverManager {
 
     private DriverManager() {}
 
-    private static ThreadLocal<WebDriver> dr = new ThreadLocal<>();
+//    private static ThreadLocal<WebDriver> dr = new ThreadLocal<>();
+    // With default value
+    private static ThreadLocal<WebDriver> dr = ThreadLocal.withInitial(() -> {
+        System.setProperty("webdriver.chrome.driver", Constants.getChromedriverpath());
+        return new ChromeDriver();
+    });
 
     public static WebDriver getDriver() {
         return dr.get();
@@ -17,6 +24,6 @@ public final class DriverManager {
     }
 
     public static void unload() {
-        dr.remove();
+        dr.remove(); // return back to default value withInitial()
     }
 }
