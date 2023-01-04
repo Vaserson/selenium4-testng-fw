6ad821410f98675f60eb735a6aacb32ffc5c8a00
Vaserson <kadabra777@gmail.com>
2023-01-04 19:23:33 +0200

    07. AssertJ Examples added

---
+++ src/test/java/AssertJClassForTest.java
@@ -0,0 +1,23 @@
+public class AssertJClassForTest {
+    private int age;
+    private String name;
+    private int year;
+
+    public AssertJClassForTest(int age, String name, int year) {
+        this.age = age;
+        this.name = name;
+        this.year = year;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
