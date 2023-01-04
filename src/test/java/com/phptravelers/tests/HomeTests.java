57930dd07105d2bab393b756d31db64300ae6e86
Vaserson <kadabra777@gmail.com>
2023-01-05 00:26:36 +0200

    07. AssertJ used in test

--- src/test/java/com/phptravelers/tests/HomeTests.java
+++ src/test/java/com/phptravelers/tests/HomeTests.java
@@ -1,17 +1,18 @@
 package com.phptravelers.tests;
 
 import driver.DriverManager;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
-import java.util.Objects;
 
-public final class HomeTests extends BaseTest{
+public final class HomeTests extends BaseTest {
 
-    private HomeTests() {}
+    private HomeTests() {
+    }
 
     @Test
     public void test1() throws InterruptedException {
@@ -19,21 +20,33 @@ public final class HomeTests extends BaseTest{
         DriverManager.getDriver().findElement(By.xpath("//ul[@class='jfHeader-menuList']/li/a[text()='Demo']")).click();
         String title = DriverManager.getDriver().getTitle();
         Thread.sleep(2000);
-        Assert.assertTrue(Objects.nonNull(title));
+
+        Assertions.assertThat(title)
+                .as("Title is null").isNotNull()
+                .as("Doesn't contain expected text").containsIgnoringCase("demo")
+                .matches("\\w.*" + "Phptravels")
+                .hasSizeBetween(15, 100);
+/*        Assert.assertTrue(Objects.nonNull(title));
         Assert.assertTrue(title.toLowerCase().contains("demo"));
 //        Assert.assertTrue(title.toLowerCase().matches("\\w.*" + "demo"));
-        Assert.assertTrue(title.length() > 15 && title.length() < 100);
+        Assert.assertTrue(title.length() > 15 && title.length() < 100);*/
+
         List<WebElement> fields = DriverManager.getDriver().findElements(By.xpath("//input"));
+        Assertions.assertThat(fields)
+                .hasSize(5)
+                .extracting(WebElement::getText)
+//                .extracting(e -> e.getText())
+                .contains("result");
         Assert.assertEquals(fields.size(), 5);
 
-        boolean isResultEnabled = false;
+/*        boolean isResultEnabled = false;
         for (WebElement field : fields) {
             if (field.getAttribute("placeholder").equalsIgnoreCase("result")) {
                 isResultEnabled = true;
                 break;
             }
         }
-        Assert.assertTrue(isResultEnabled);
+        Assert.assertTrue(isResultEnabled);*/
     }
 
     @Test(enabled = false)
