2b160ce88165f16b0110b849fe299ab0e42ac54a
Vaserson <kadabra777@gmail.com>
2023-01-14 10:50:47 +0200

    14. DataProvider in LoginTests with Parallel and parallel count 3 in testng.xml

--- src/test/java/framework/orangeHRM/tests/LoginTests.java
+++ src/test/java/framework/orangeHRM/tests/LoginTests.java
@@ -4,35 +4,46 @@ import framework.BaseTest;
 import framework.orangeHRM.pages.HomePage;
 import framework.orangeHRM.pages.LoginPage;
 import org.assertj.core.api.Assertions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public final class LoginTests extends BaseTest {
 
     private LoginTests(){}
 
-    @Test
-    public void loginTest1() {
+    @DataProvider(name = "LoginTestDataProvider", parallel = true) //!!! Set thread count in the testNG.xml <suite name="Suite" data-provider-thread-count="3">
+    public Object[][] getData() {
+        return new Object[][] {
+                {"Admin", "admin123"},
+                {"Admin123", "admin"},
+                {"Admin", "admin123"},
+                {"Admin", "admin123"}
+        };
+    }
+
+    @Test(dataProvider = "LoginTestDataProvider")
+    public void loginTest1(String username, String password) {
         LoginPage loginPage = new LoginPage();
-        loginPage.enterUsername("Admin")
-                .enterPassword("admin123")
+        loginPage.enterUsername(username)
+                .enterPassword(password)
                 .clickSubmitBtn()
                 .clickName()
                 .clickLogoutBtn();
     }
 
-    @Test
-    public void loginTest2() {
+    @Test(dataProvider = "LoginTestDataProvider")
+    public void loginTest2(String username, String password) {
         LoginPage loginPage = new LoginPage();
         HomePage homePage = loginPage
-                .enterUsername("Admin").enterPassword("admin123").clickSubmitBtn();
+                .enterUsername(username).enterPassword(password).clickSubmitBtn();
         homePage
                 .clickName().clickLogoutBtn();
     }
 
-    @Test
-    public void loginTest3() {
+    @Test(dataProvider = "LoginTestDataProvider")
+    public void loginTest3(String username, String password) {
         String title = new LoginPage()
-                .enterUsername("Admin").enterPassword("admin123").clickSubmitBtn()
+                .enterUsername(username).enterPassword(password).clickSubmitBtn()
                 .clickName().clickLogoutBtn()
                 .getTitle();
 
